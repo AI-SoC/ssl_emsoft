4408f15a32261fbefa425abbe5f47cee74c06741
Ssong0410 <ssong98@kaist.ac.kr>
2022-11-08 11:48:08 +0900

    add 02

---
+++ day2/students/99_seokchan/02_struct/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct {
+    unsigned char CNFG1;
+    unsigned char CNFG2;
+    unsigned char MODE;
+} ADC_CONFIG;
+
+int main() {
+    printf("Running...\n")  ;
+
+    int a = 0x12345678      ;
+
+    int* ap = &a ; 
+    printf("a is 0x%8X at %p\n", *ap, ap);
+
+    char* abp = (char*)&a   ; 
+
+    printf("a is 0x%X at %p\n", *abp, abp);
+
+    *(abp + 2)  = 0x5A ; 
+    printf("a is 0x%8X at %p\n", *ap, ap);
+
+    return 0;
+}
