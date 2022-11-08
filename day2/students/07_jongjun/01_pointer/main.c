dfc5695d04b8fd73b55a89a391fee466e5976d30
Jongjunp <pjjkey@kaist.ac.kr>
2022-11-08 11:44:17 +0900

    day2 endian & modification

--- day2/students/07_jongjun/01_pointer/main.c
+++ day2/students/07_jongjun/01_pointer/main.c
@@ -7,7 +7,12 @@ int main() {
 
     int* ap = &a;
     printf("a is 0x%8X at %p\n", *ap, ap);
+
     char* abp = (char*)&a;
+    printf("a is 0x%X at 0x%p\n", *abp, abp);
+
+    *(abp+2) = 0x5A;
+    printf("a is 0x%X at 0x%p\n", *ap, ap);
 
     return 0;
 }
